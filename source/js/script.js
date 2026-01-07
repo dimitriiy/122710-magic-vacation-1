f18c397988e8d0f3f6ffe9cb064c6d86a0b273d0
Dmitry.Mazitov <dmitry.mazitov@x5.ru>
2026-01-07 11:47:15 +0300

    task-1

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,16 @@
 // modules
-import mobileHeight from './modules/mobile-height-adjust.js';
-import slider from './modules/slider.js';
-import menu from './modules/menu.js';
-import footer from './modules/footer.js';
-import chat from './modules/chat.js';
-import result from './modules/result.js';
-import form from './modules/form.js';
-import social from './modules/social.js';
-import FullPageScroll from './modules/full-page-scroll';
+import mobileHeight from "./modules/mobile-height-adjust.js";
+import slider from "./modules/slider.js";
+import menu from "./modules/menu.js";
+import footer from "./modules/footer.js";
+import chat from "./modules/chat.js";
+import result from "./modules/result.js";
+import form from "./modules/form.js";
+import social from "./modules/social.js";
+import FullPageScroll from "./modules/full-page-scroll";
 
 // init modules
+load();
 mobileHeight();
 slider();
 menu();
@@ -21,3 +22,9 @@ social();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
+
+function load() {
+  window.addEventListener(`load`, () => {
+    document.body.classList.add(`loaded`);
+  });
+}
