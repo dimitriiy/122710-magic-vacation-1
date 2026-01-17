a1ae788728a5c57855004a2a75149ca4c45b19f3
Dmitry.Mazitov <dmitry.mazitov@x5.ru>
2026-01-17 20:09:28 +0300

    upd

--- source/js/script.js
+++ source/js/script.js
@@ -28,3 +28,50 @@ function load() {
     document.body.classList.add(`loaded`);
   });
 }
+
+class AccentTypographyBuild {
+  constructor(elementSelector, duration, minDelay, maxDelay) {
+    this.element = elementSelector;
+    this.duration = duration;
+    this.minDelay = minDelay;
+    this.maxDelay = maxDelay;
+
+    this.classForActivate = `accent-typography--active`;
+    this.prepareText();
+  }
+
+  prepareText() {
+    const textContent = this.element.textContent.trim();
+    const words = textContent.split(/\s/);
+    const content = words.reduce((acc, word) => {
+      const row = document.createElement(`span`);
+      row.classList.add(`accent-row`);
+
+      word.split(``).forEach((letter) => {
+        const span = document.createElement(`span`);
+        span.classList.add(`accent-item`);
+        console.log(this.getDelay());
+        span.style.animationDelay = this.getDelay();
+        span.textContent = letter;
+
+        row.appendChild(span);
+      });
+
+      acc.appendChild(row);
+
+      return acc;
+    }, document.createDocumentFragment());
+    this.element.innerHTML = ``;
+    this.element.appendChild(content);
+  }
+
+  getDelay() {
+    return `${getRandomFloat(this.minDelay, this.maxDelay)}s`;
+  }
+}
+
+function getRandomFloat(min = 0, max = 0.4) {
+  return Math.random() * (max - min) + min;
+}
+
+new AccentTypographyBuild(document.querySelector(`.intro__title`));
